1dcbcd1b15cd694239331d4a5c0f7d6ee5b50bcb
saninduhansara <e22130@eng.pdn.ac.lk>
2026-02-15 15:27:29 +0530

    installing dependency body-parser

--- code/backend/index.js
+++ code/backend/index.js
@@ -1,9 +1,13 @@
 import express from "express"
 import mongoose from "mongoose"
 import dotenv from "dotenv"
+import bodyParser from "body-parser"
 dotenv.config()
+
+
 const app = express()
 
+app.use(bodyParser.json())
 
 const connectionString = process.env.MONGO_URI
 
