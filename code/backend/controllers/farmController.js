f3b6ca7893683f659c9743b7f81d65e8053c5aaf
saninduhansara <e22130@eng.pdn.ac.lk>
2026-02-16 19:44:53 +0530

    Auto increment farm id 📈

--- code/backend/controllers/farmController.js
+++ code/backend/controllers/farmController.js
@@ -7,6 +7,20 @@ export async function createFarm(req, res) {
         return res.status(403).json({ message: "Access denied. Admins only" });
     }
 
+
+    let farmId = "FAM00202"
+    const latestid = await Farm.find().sort({ createdDate: -1 }).limit(1);
+
+    if(latestid.length > 0){
+        const lastfarmIdString = latestid[0].farmId
+        const lastfarmIdWithoutPrefix = lastfarmIdString.replace("FAM","")
+        const lastfarmIdInInteger = parseInt(lastfarmIdWithoutPrefix)
+        const newfarmIdInInteger = lastfarmIdInInteger + 1
+        const newfarmIdWithoutPrefix = newfarmIdInInteger.toString().padStart(5,"0")
+        farmId = "FAM" + newfarmIdWithoutPrefix
+
+    }
+
     const { farmerNIC, ...farmData } = req.body;
 
     // Find the farmer by NIC
@@ -17,7 +31,8 @@ export async function createFarm(req, res) {
 
     const farm = new Farm({
         ...farmData,
-        farmer: farmer._id
+        farmer: farmer._id,
+        farmId: farmId
     });
 
     try {
